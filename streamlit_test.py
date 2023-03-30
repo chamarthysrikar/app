498eb9de063f8d4a3f7a6b6923ed011a09b5762b
chamarthysrikar <53967980+chamarthysrikar@users.noreply.github.com>
2023-03-30 15:19:14 -0400

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -3,18 +3,9 @@ from streamlit_webrtc import webrtc_streamer
 from streamlit_webrtc import WebRtcMode
 st.title("Audio Driven Talking demo")
 
-import av
-import cv2
 
 col1, col2 = st.columns(2)
 
-
-def callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-
-    img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 with col1:
     ctx=webrtc_streamer(key="sample")
@@ -26,7 +17,6 @@ with col2:
                 "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
             },
             media_stream_constraints={"video": True, "audio": True},
-            video_frame_callback=callback,
             source_video_track=ctx.input_video_track,
             desired_playing_state=ctx.state.playing,
         )
